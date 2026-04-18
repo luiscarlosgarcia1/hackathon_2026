8e3c9d50cd83f0a6db85a80c7c2765f701742a81
BabbleFG <quiroga.gabriel108801@gmail.com>
2026-04-18 15:25:36 -0500

    Youtube Transcrtipt

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 
-
-class DevelopmentConfig:
+class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///hearings.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
+
+class DevelopmentConfig(Config):
+    pass
